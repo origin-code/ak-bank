47941df56b74158abcc094450ba1065454a6eb60
wearehereagain23 <wearehereagain23@gmail.com>
2025-10-09 10:30:27 -0700

    wucrf

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     FIREBASE: {
-        apiKey: "AIzaSyBKOGvOb_plKMaH_oec_E5wSY0shY27Z9E",
-        authDomain: "ge-lite.firebaseapp.com",
-        projectId: "ge-lite",
-        storageBucket: "ge-lite.appspot.com",
-        messagingSenderId: "1004534306152",
-        appId: "1:1004534306152:web:02b96781116ecdd70f72dd"
+        apiKey: "AIzaSyAn5Rd8BKfiY1XzsFPos6BgNoBqz0TnznQ",
+        authDomain: "mailalerttem.firebaseapp.com",
+        projectId: "mailalerttem",
+        storageBucket: "mailalerttem.appspot.com",
+        messagingSenderId: "1073275971845",
+        appId: "1:1073275971845:web:95386ebd36c01facfc6f6c"
     },
     SUPABASE_URL: 'https://tkolyezukxoefqjzhqar.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrb2x5ZXp1a3hvZWZxanpocWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTU5MTMsImV4cCI6MjA2NzQzMTkxM30.S9flBLlO7p9IRA7XPZBMpc9lCC09PtveURimGeY0Nto',
